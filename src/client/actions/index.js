cb030cf15a28df1e6ed1d0fea0d628d07284438c
wuyang910217 <wuyang910217@gmail.com>
2017-10-22 23:19:46 +0800

    add proxy & set cookie on server to identify user

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,10 +1,8 @@
-import axios from 'axios';
-
 export const FETCH_USERS = 'fetch_users';
 // 用户直接从路由进入/users，会执行，前台渲染后，会再次执行
 // 但是不能取消，如果取消，用户从前台切换链接，不会执行服务器端渲染，会导致没有数据
-export const fetchUsers = () => async dispatch => {
-  const res = await axios.get('http://react-ssr-api.herokuapp.com/users');
+export const fetchUsers = () => async (dispatch, getState, api) => {
+  const res = await api.get('/users');
   dispatch({
     type: FETCH_USERS,
     payload: res
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import 'babel-polyfill';
 import express from 'express';
 import { matchRoutes } from 'react-router-config';
+import proxy from 'express-http-proxy';
 import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/store';
 
 const app = express();
 
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+      opts.headers['x-forwarded-host'] = 'localhost:3000';
+      return opts;
+    }
+  })
+);
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  const store = createStore();
+  const store = createStore(req);
 
   const routes = matchRoutes(Routes, req.path);
   const promises = routes.map(({ route }) => {
